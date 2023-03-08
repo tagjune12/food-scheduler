48eb58541efb807ba276de60e96e429159f9a27e
tagjune12 <tagjune12@google.com>
2023-03-09 01:33:53 +0900

    마커 클릭 이벤트 구현

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,38 +2,67 @@ import React, { useEffect, useRef } from 'react';
 import '@components/Map.css';
 // import { Helmet } from 'react-helmet';
 import restaurants from '@data/restaurants.json';
+import RestaurantCard from '@components/RestaurantCard';
 
 const Map = () => {
   useEffect(() => {
+    // 지도 중앙좌표 설정
     const center: naver.maps.LatLng = new naver.maps.LatLng(
       37.4028207, // y
       127.1115201, // x
     );
-
+    // 중앙 좌표를 중심으로 하는 지도 생성
     const map: naver.maps.Map = new naver.maps.Map('map', {
       center: center,
       zoom: 16, // default
     });
 
-    restaurants.forEach((restaurant, index) => {
+    const markers: naver.maps.Marker[] = [];
+
+    restaurants.forEach((restaurant) => {
       const { position } = restaurant;
       const markerPosition: naver.maps.LatLng = new naver.maps.LatLng(
         parseFloat(position.y), // y
         parseFloat(position.x), // x
       );
+      // 지도위에 좌표 설정
+      markers.push(
+        new naver.maps.Marker({
+          position: markerPosition,
+          map: map,
+        }),
+      );
+    });
+    const infoWindows: naver.maps.InfoWindow[] = [];
 
-      const marker = new naver.maps.Marker({
-        position: markerPosition,
-        map: map,
-      });
-
-      console.log(index);
+    restaurants.forEach((restaurant, index) => {
+      infoWindows.push(
+        new naver.maps.InfoWindow({
+          content: '<div id="info-window">테스트<div/>',
+        }),
+      );
+      // infoWindows.at(-1)?.open(map, markers[index]);
     });
 
-    // const marker = new naver.maps.Marker({
-    //   position: center,
-    //   map: map,
-    // });
+    // 정보창은 하나밖에 활성화가 안된다
+    // 여러개 띄우고 싶으면 오버레이로 구현해야함
+    infoWindows[0]?.open(map, markers[0]);
+    // infoWindows[1]?.open(map, markers[1]);
+    // infoWindows[2]?.open(map, markers[2]);
+
+    for (let i = 0; i < markers.length; i++) {
+      naver.maps.Event.addListener(markers[i], 'click', () => {
+        infoWindows[i].open(map, markers[i]);
+        console.log('marker is clicked');
+      });
+
+      // 이벤트 리스너 붙여도 빌드하면 안붙어있음
+      naver.maps.Event.addListener(infoWindows[i], 'click', () => {
+        console.log('info window is clicked');
+      });
+    }
+
+    console.log('useEffect is work');
   }, []);
 
   const mapStyle = {
